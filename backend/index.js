42d9032ef7db5289838dad5c7848fbe40336ab37
Prashant Dey <prashantsavior@gmail.com>
2023-06-13 18:41:18 +0530

    ADD: completed backend of travel memory

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,16 @@
 const express = require('express')
+const cors = require('cors')
 require('dotenv').config()
 
 const app = express()
 PORT = process.env.PORT
 const conn = require('./conn')
 app.use(express.json())
+app.use(cors())
+
+const tripRoutes = require('./routes/trip.routes')
+
+app.use('/trip', tripRoutes) // http://localhost:3001/trip --> POST/GET/GET by ID
 
 app.get('/hello', (req,res)=>{
     res.send('Hello World!')
